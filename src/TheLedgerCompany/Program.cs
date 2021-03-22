e1297d58e4cb391b5490b1b9e4ddf3ee8332c29a
Sooraj Parameswaran <srjpn123@gmail.com>
2021-03-22 12:05:12 +0530

    inital commit

---
+++ src/TheLedgerCompany/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheLedgerCompany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ test/TheLedgerCompany.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace TheLedgerCompany.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
